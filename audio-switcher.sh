3bf5d8758a7ed4cec8510ff7d926d3426afa3c7e
sivadnivek <sivadnivek@gmail.com>
2026-01-13 08:38:33 -0500

    Update audio-switcher.sh
    k bukkkasdfjkl;;;dkdkdkdkdkdkdki thinkasdjfkl;asdfjkl;asdfjkl;asdfjkl;asdjfkl;asdjfkl;asdfjkl;asdfjkl;asdjfkl;asdfjkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkasd

--- audio-switcher.sh
+++ audio-switcher.sh
@@ -1,13 +1,14 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# get the list of available sinks and their indexes
-mapfile -t sinks < <(pacmd list-sinks | awk '/index/ {print $NF}')
+# get the list of available sinks and their indexes ## added the grep to remove something specific to my environment which is a mic loopback
+# basically just changed all the commands to be compatible with pactl instead of pacmd
+mapfile -t sinks < <(pactl list sinks | grep -v MV7 | awk '/index/ {print $NF}')
 
 # get all sink input indexes
-mapfile -t input_indexes < <(pacmd list-sink-inputs | awk '/index/ {print $NF}')
+mapfile -t input_indexes < <(pactl list sink-inputs | awk '/index/ {print $NF}')
 
 # find the active sink index
-active_sink_index=$(pacmd list-sinks | awk '/\* index/ {print $NF}')
+active_sink_index=$(pactl list sinks | awk '/\* index/ {print $NF}')
 
 # find the index of the next sink in the list
 next_sink_index=${sinks[0]}
@@ -19,7 +20,9 @@ for ((i = 0; i < ${#sinks[@]} - 1; i++)); do
 done
 
 # switch to the next sink
-pacmd set-default-sink "$next_sink_index" &> /dev/null
+pactl set-default-sink "$next_sink_index" &> /dev/null
 for input_index in "${input_indexes[@]}"; do
-    pacmd move-sink-input "$input_index" "$next_sink_index" &> /dev/null
+    pactl move-sink-input "$input_index" "$next_sink_index" &> /dev/null
 done
+
+#i think this should wor
